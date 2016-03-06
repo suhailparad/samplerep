ddf6a5cbf0bd9b4e66d2273e78db53ff3ef0df58
suhail <pk.suhail47@gmail.com>
2016-03-06 21:25:24 +0530

    new commit

--- config/database.php
+++ config/database.php
@@ -56,6 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
+            'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
